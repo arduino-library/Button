ba235ce4b39bb5bf07ebc5e134c24d1727dfb71f
Karim Hraibi <khraibi@gmail.com>
2020-03-12 10:15:31 +0100

    Initial commit

---
+++ Button.cpp
@@ -0,0 +1,96 @@
+/* 
+ * Push button control
+ *
+ * This source file is part of the Lithium-Ion Battery Charger Arduino firmware
+ * found under http://www.github.com/microfarad-de/Button
+ * 
+ * Please visit:
+ *   http://www.microfarad.de
+ *   http://www.github.com/microfarad-de
+ * 
+ * Copyright (C) 2019 Karim Hraibi (khraibi at gmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+#include "Button.h"
+
+
+#define BUTTON_LONG_PRESS_TIMEOUT (500)
+
+
+void ButtonClass::press (void) {
+  wasPressed = pressed;
+  pressed = true;
+}
+
+void ButtonClass::release (void) {
+  wasPressed = pressed;
+  pressed = false;
+}
+
+bool ButtonClass::rising (void) {
+  bool rv = false;
+  if (pressed && !wasPressed) {
+    wasPressed = pressed;
+    longPressTs = millis ();
+    longPressed = true;
+    wasLongPressed = false;
+    rv = true;
+  }
+  return rv;
+}
+
+bool ButtonClass::falling (void) {
+  bool rv = false;
+  if (!pressed && wasPressed && !wasLongPressed) {
+    wasPressed = pressed;
+    rv = true;
+  }
+  return rv;  
+}
+
+bool ButtonClass::fallingLongPress (void) {
+  bool rv = false;
+  if (!pressed && wasPressed && wasLongPressed) {
+    wasPressed = pressed;
+    wasLongPressed = false;
+    rv = true;
+  }
+  return rv;  
+}
+
+bool ButtonClass::fallingContinuous (void) {
+  return !pressed && wasPressed; 
+}
+
+bool ButtonClass::longPress (void) {
+  bool rv = false;
+  if (pressed && longPressed && millis () - longPressTs > BUTTON_LONG_PRESS_TIMEOUT) {
+    longPressed = false;
+    wasLongPressed = true;
+    rv = true;
+  }
+  return rv;
+}
+
+bool ButtonClass::longPressContinuous (void) {
+  bool rv = false;
+  if (pressed && millis () - longPressTs > BUTTON_LONG_PRESS_TIMEOUT) {
+    longPressed = false;
+    wasLongPressed = true;
+    rv = true;
+  }
+  return rv;
+}
---
+++ Button.h
@@ -0,0 +1,58 @@
+/* 
+ * Push button control
+ *
+ * This source file is part of the Lithium-Ion Battery Charger Arduino firmware
+ * found under http://www.github.com/microfarad-de/Button
+ * 
+ * Please visit:
+ *   http://www.microfarad.de
+ *   http://www.github.com/microfarad-de
+ * 
+ * Copyright (C) 2019 Karim Hraibi (khraibi at gmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+#ifndef __BUTTON_H
+#define __BUTTON_H
+
+#include <Arduino.h>
+
+
+/*
+ * Push button implementation class
+ */
+class ButtonClass {
+  public:
+    void press (void);
+    void release (void);
+    bool rising (void);
+    bool falling (void);
+    bool fallingLongPress (void);
+    bool fallingContinuous (void);
+    bool longPress (void); 
+    bool longPressContinuous (void); 
+
+    bool pressed = false;
+    bool wasPressed = false;
+    
+  private:
+    bool longPressed = false;
+    bool wasLongPressed = false;
+    uint32_t longPressTs = 0;
+};
+
+
+
+#endif // __BUTTON_H
